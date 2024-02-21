578920d733d29987fdae0606481b6f1b65265bec
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-02-21 02:02:08 +0100

    kelvinToCelsius added. theme added.

---
+++ src/common/themes/theme.ts
@@ -0,0 +1,5 @@
+import {Float} from 'react-native/Libraries/Types/CodegenTypes';
+
+export const theme = {
+  bgWhite: (opacity: Float) => `rgba(255,255,255, ${opacity})`,
+};
---
+++ src/common/utils/index.ts
@@ -0,0 +1,3 @@
+export const kelvinToCelsius = (kelvin:number) => {
+    return kelvin - 273.15; // Kelvin'i Celsius'a dönüştürmek için 273.15 çıkarılır
+  };
