fbbdacd565320790207514326c5109822adf0061
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-02-22 01:22:10 +0100

    calculateSunRiseTime (utils) added.

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,11 @@
-export const kelvinToCelsius = (kelvin:number) => {
-    return kelvin - 273.15; // Kelvin'i Celsius'a dönüştürmek için 273.15 çıkarılır
-  };
+export const kelvinToCelsius = (kelvin: number) => {
+  return kelvin - 273.15; // Kelvin'i Celsius'a dönüştürmek için 273.15 çıkarılır
+};
+
+export const calculateSunRiseTime = (time: number) => {
+  const sunriseDate = time ? new Date(time * 1000) : null;
+  const sunriseTimeString = sunriseDate
+    ? sunriseDate.toLocaleTimeString()
+    : 'Bilinmiyor';
+  return sunriseTimeString;
+};
