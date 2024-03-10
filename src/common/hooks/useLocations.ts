1f35b8eaeb49fb89657e11a8b2bc48ddfd254df5
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-03-11 00:32:54 +0100

    location refresh added.

--- src/common/hooks/useLocations.ts
+++ src/common/hooks/useLocations.ts
@@ -1,31 +1,44 @@
 import { useState, useEffect } from "react";
-import Geolocation from "@react-native-community/geolocation";
+import Geolocation, { GeolocationError, GeolocationResponse } from "@react-native-community/geolocation";
 
 interface GeolocationData {
   latitude: number;
   longitude: number;
 }
 
-export const useGeolocation = (): [string, GeolocationData] => {
-  const [error, setError] = useState("");
+export const useGeolocation = (): [string, GeolocationData, () => void] => {
+  const [error, setError] = useState<string>('');
   const [position, setPosition] = useState<GeolocationData>({
     latitude: 0,
     longitude: 0
   });
 
-  useEffect(() => {
-    const watchId = Geolocation.watchPosition(
-      pos => {
-        setError("");
+  const refresh = () => {
+    Geolocation.getCurrentPosition(
+      (pos: GeolocationResponse) => {
+        setError('');
         setPosition({
           latitude: pos.coords.latitude,
           longitude: pos.coords.longitude
         });
       },
-      e => setError(e.message)
+      (e: GeolocationError) => setError(e.message)
+    );
+  };
+
+  useEffect(() => {
+    const watchId = Geolocation.watchPosition(
+      (pos: GeolocationResponse) => {
+        setError('');
+        setPosition({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude
+        });
+      },
+      (e: GeolocationError) => setError(e.message)
     );
     return () => Geolocation.clearWatch(watchId);
   }, []);
 
-  return [error, position];
+  return [error, position, refresh];
 };
