0147bcb4aaaa9e2136ab5537ce1fd064092cbc35
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-03-11 00:32:25 +0100

    IOS app icon added & name changed.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import AppNavigator from './src/navigations/AppNavigator';
 
-
 function App(): React.JSX.Element {
 
   return (
